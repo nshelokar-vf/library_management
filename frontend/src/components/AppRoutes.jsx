ea466c95e284926f5319be280c37f9f4d767a2bf
Nupur Shelokar <nshelokar@verticalfox.com>
2024-07-16 12:31:21 +0530

    implemented edit feature

--- frontend/src/components/AppRoutes.jsx
+++ frontend/src/components/AppRoutes.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import NewBookForm from "../features/books/NewBookForm"
 import BookDetails from "../features/books/BookDetails"
+import EditBookForm from "../features/books/EditBookForm"
 
 
 function AppRoutes(){
@@ -11,6 +12,8 @@ function AppRoutes(){
             <Route path="/" element={<BookList/>}/>
             <Route path="/new" element={<NewBookForm/>}/>
             <Route path="books/:id" element={<BookDetails/>}/>
+            <Route path="books/:id/edit" element={<EditBookForm/>}/>
+
         </Routes>
     )
 }
